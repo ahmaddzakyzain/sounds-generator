c276d1a5cf3b8ab32f750b0b423e327182d507b2
ahmaddzakyzain <adzain15.adz@gmail.com>
2016-12-13 01:04:27 -0800

    Add files via upload

---
+++ tone.ino
@@ -0,0 +1,47 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8, OUTPUT);
+}
+
+void loop() {
+ // put your main code here, to run repeatedly:
+sol();jeda();diam();sol();jeda();diam();sol();jeda();jeda();mi();jeda();Do();jeda();diam();Do();jeda();diam(); Do();jeda();diam();jeda();
+Do();jeda();sol();jeda;fa();jeda();mi();jeda();jeda();re();jeda();jeda();jeda();
+fa();jeda();fa();jeda();fa();jeda();jeda();re();jeda();si();jeda();diam();si();jeda();diam();
+si();jeda();diam();la();jeda();sol();jeda();la();jeda();fa();jeda();
+sol();jeda();mi();jeda();jeda();diam();sol();jeda();sol();jeda();sol();jeda();jeda();mi();jeda();Do();jeda();diam();Do();jeda();diam(); Do();jeda();diam();jeda();
+Do();jeda();sol();jeda;fa();jeda();d0();jeda();la();jeda();jeda();diam();  
+jeda();jeda();la();jeda();la();jeda();la();jeda();Do();jeda();sol();jeda();
+sol();jeda();sol();jeda();jeda();diam();sol();jeda();fa();jeda();
+mi();jeda();re();jeda();jeda();diam();
+
+}
+void d0()
+{tone(8,523);//c1
+}
+void re()
+{tone(8,587);//d2
+}
+void mi()
+{tone(8,659);//e3
+}
+void fa()
+{tone(8,698);//f4
+}
+void sol()
+{tone(8,784);//g5
+}
+void la()
+{tone(8,880);//a6
+}
+void si()
+{tone(8,988);//b7
+}
+void Do()
+{tone(8,1047);//c1
+}
+void diam()
+{ noTone(8);delay(50);
+}
+void jeda()
+{ delay(500);}
